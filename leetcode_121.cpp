fc19ac85e7f3b9c66404fd6f08a5fe90987cb9c5
hantongliu <hantongliu@bytedance.com>
2022-02-11 11:56:05 +0800

    2022_02_11

---
+++ leetcode_121.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+      int max_profit = 0;
+      int min_price = prices[0];
+      for(int i = 1; i < prices.size(); ++i) {
+        if (prices[i] - min_price > max_profit)
+          max_profit = prices[i] - min_price;
+        min_price = min(min_price, prices[i]);
+      }
+      return max_profit;
+    }
+};
